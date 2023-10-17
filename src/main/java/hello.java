74735a17315d1962c1c21da4ff58c4ba63df59c1
rakesh <rakesh.dharshanam333@gmail.com>
2023-10-16 23:02:36 -0400

    third commit with logger

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,7 +1,15 @@
+import java.util.logging.Logger;
+
+
 
 public class hello {
+	
+	static Logger logger = Logger.getLogger(hello.class.getName());
 
 	public static void main(String[] args) {
+
+		logger.info("hello from java");
+		
 		System.out.println("hello form java");
 	}
 	
